2dc28fcd016251bcaf0a41a7c5e05e0d6d75d23c
Jifeng Zhang <zjfroot@gmail.com>
2013-03-11 14:53:44 +0100

    Created links in index.html

--- whatis2links.py
+++ whatis2links.py
@@ -3,5 +3,9 @@ import sys
 
 line = sys.stdin.readline()
 while line:
-    print line.split()
+    words = line.split()
+    if len(words) > 0:
+        man_page_name = words[0]
+        link = "<a href=\"./osx10.8/" + man_page_name.replace("(", ".").replace(")", ".") + "html\">" + man_page_name + "</a>"
+        print line.replace(man_page_name, link).rstrip()
     line = sys.stdin.readline()
