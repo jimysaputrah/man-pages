df4b06a934fc0be81ff3c476210f1274779e5a59
Jifeng Zhang <zjfroot@gmail.com>
2013-03-20 23:18:56 +0100

    Added a debug line to man2html.py

--- man2html.py
+++ man2html.py
@@ -9,6 +9,7 @@ for root, _, files in os.walk(path):
         fullpath = os.path.join(root, f)
         basename = os.path.basename(fullpath)
         if basename != "whatis":
+            print "Processing: " + basename
             new_basename = basename.replace(".gz", "") + ".html"
             call("man " + fullpath + " |./man2html >" + outputdir + "/" + new_basename, shell=True)
         #print fullpath
