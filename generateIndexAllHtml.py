b87d387641175338e9c3b03659e96f1a37438bfe
Jifeng Zhang <zjfroot@gmail.com>
2013-03-20 21:58:08 +0100

    Added a script that generate a html page that from folder html/

---
+++ generateIndexAllHtml.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import os
+
+
+def rreplace(s, old, new, occurrence):
+    li = s.rsplit(old, occurrence)
+    return new.join(li)
+
+
+inputdir = "html/osx10.8"
+for root, _, files in os.walk(inputdir):
+    for f in files:
+        fullpath = os.path.join(root, f)
+        basename = os.path.basename(fullpath)
+        print "<a href=\"osx10.8/" + basename + ">" + rreplace(basename[:-5], ".", "(", 1) + ")" + "</a>"
