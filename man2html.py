d0d4fd71cc69889e45568138383d36f57d9b4124
Jifeng Zhang <zjfroot@gmail.com>
2013-03-20 22:36:47 +0100

    Added a script to use man2html util to get better looking html

---
+++ man2html.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import os
+from subprocess import call
+
+path = "/usr/share/man/"
+outputdir = "html/osx10.8"
+for root, _, files in os.walk(path):
+    for f in files:
+        fullpath = os.path.join(root, f)
+        basename = os.path.basename(fullpath)
+        if basename != "whatis":
+            new_basename = basename.replace(".gz", "") + ".html"
+            call("man " + fullpath + " |./man2html >" + outputdir + "/" + new_basename, shell=True)
+        #print fullpath
